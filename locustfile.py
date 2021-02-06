4d3ce62b5f5f119c1519da81ac83418a2268440f
fbamuse <fukuhara.masanori@gmail.com>
2021-02-06 15:36:59 +0000

    locust

--- locustfile.py
+++ locustfile.py
@@ -1,22 +1,10 @@
-from locust import HttpLocust, TaskSet, task
-import random
-import string
+from locust import HttpUser, TaskSet, task, between, constant
 
-class GreetingTaskSet(TaskSet):
-    def on_start(self):
-        self.client.headers = {'Content-Type': 'application/json; charset=utf-8'}
+class UserBehavior(TaskSet):
+    @task(1)
+    def profile(self):
+        self.client.get("/sample", verify=False)
 
-    @task
-    def fetch_greeting(self):
-        self.client.get("/greetings")
-
-    @task
-    def create_greeting(self):
-        # 雑にランダム文字列で済ませちゃう。 (注: random.choices は 3.6 以降でしか使えません)
-        self.client.post("/greetings", json={"text": ''.join(random.choices(string.ascii_letters, k=10))})
-
-class GreetingLocust(HttpLocust):
-    task_set = GreetingTaskSet
-
-    min_wait = 100
-    max_wait = 1000
+class WebsiteUser(HttpUser):
+    tasks = {UserBehavior:1}
+    wait_time = constant(0)
