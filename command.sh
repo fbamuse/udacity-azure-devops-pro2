2a90db5f86b223cb49f863433b44e47caa3c8b66
fbamuse <fukuhara.masanori@gmail.com>
2021-02-13 10:51:49 +0000

    docker

--- command.sh
+++ command.sh
@@ -5,7 +5,7 @@ source ~/venv/bin/activate
 pip install -r requirements.txt
 
 
-
+# export pass=""""
 az group create -l japaneast -n  udagroup
 az appservice plan create -g udagroup -n my-service-plan --is-linux --sku B1  
-az webapp create -g udagroup -p my-service-plan -n my-appservice --runtime "Python|3.6"  
+az webapp create -g udagroup  -p my-service-plan -n my-appservice -i udacity-azure-devops -s bamuse -w $pass
